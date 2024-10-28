191284a4ae3566df5cfb867095a50922f63d1465
Maya Shavin <dpnminh@gmail.com>
2024-10-28 20:45:32 +0200

    update: telemetry

--- src/utils/telemetry.ts
+++ src/utils/telemetry.ts
@@ -1,6 +1,7 @@
 import TelemetryReporter from "@vscode/extension-telemetry";
 
-const connectionString = process.env.TELEMETRY;
+const connectionString =
+  "InstrumentationKey=51d85845-531b-4bc4-8de9-c71c67549782;IngestionEndpoint=https://centralus-2.in.applicationinsights.azure.com/;LiveEndpoint=https://centralus.livediagnostics.monitor.azure.com/;ApplicationId=d03de407-6132-4bbd-8c92-f8c68059c894";
 
 let reporter: TelemetryReporter | null = null;
 
